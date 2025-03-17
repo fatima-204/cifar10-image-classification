d64fb915d9fec453bdd5652e5a6445f686d804f4
fatima-204 <fatimat2440@gmail.com>
2025-03-18 00:44:46 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2ab7be05ccdc9561549fb6320caa8f83f3ea1349557dd7c8f7652668936962e0
+size 2392
---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0abfd3ce653f29171cbbdd58cde29e31cdebb91050f395c730c6ef814dea9c1e
+size 715
